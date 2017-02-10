ccd8673f6a380d674ac1866bb3db8afee2aa1e37
Sabbir-Ahmed <sabbir044@gmail.com>
2017-02-10 23:55:51 +0100

    Refactor code and added keyboard for numkeys, operators and enter(return)

--- index.js
+++ index.js
@@ -36,45 +36,70 @@ function process() {
 
 Array.from(numpad).forEach(function(element) {
     element.addEventListener('click', () => {
-        input[ currentNumber ] = input[ currentNumber ] * 10 + parseInt(element.value);
-        isValidInput[ currentNumber ] = true;
-        display.value += element.value;
+        addDigitToCurrentNumber(parseInt(element.value));
+        element.blur();
 }, false)});
+
+function addDigitToCurrentNumber(digit) {
+    input[ currentNumber ] = input[ currentNumber ] * 10 + digit;
+    isValidInput[ currentNumber ] = true;
+    display.value = display.value.concat(digit);
+}
 
 Array.from(opPad).forEach(function(element) {
     element.addEventListener('click', () => {
-
-        if(isValidInput[0]==false) {
-            //DO NOTHING
-        } else if(isValidInput[0]==true && isValidInput[1]==false) {
-            currentNumber = 1;
-            operator = setOperator(element.value);
-            display.value += element.value;
-        } else if(isValidInput[0]==true && isValidInput[1]==true) {
-            result = process();
-            resultVal.value = result;
-            input[ 0 ] = result;
-            input[ 1 ] = 0;
-            isValidInput[1] = false;
-            operator = setOperator(element.value);
-            currentNumber = 1;
-            display.value = result.toString();
-        }
+        processOperator(element.value);
+        element.blur();
 }, false)});
 
+function processOperator(operator_str) {
+    if(isValidInput[0]==false) {
+        //DO NOTHING
+    } else if(isValidInput[0]==true && isValidInput[1]==false) {
+        currentNumber = 1;
+        operator = setOperator(operator_str);
+        display.value = display.value.concat(operator_str);
+    } else if(isValidInput[0]==true && isValidInput[1]==true) {
+        result = process();
+        resultVal.value = result;
+        input[ 0 ] = result;
+        input[ 1 ] = 0;
+        isValidInput[1] = false;
+        operator = setOperator(operator_str);
+        currentNumber = 1;
+        display.value = result.toString().concat(operator_str);
+    }
+}
+
 document.getElementById("btn_eq").addEventListener("click", () => {
+    processEqual();
+});
+
+function processEqual() {
     if(isValidInput[0]==true && isValidInput[1]==true) {
-            result = process();
-            resultVal.value = result;
-            input[ 0 ] = result;
-            input[ 1 ] = 0;
-            isValidInput[1] = false;
-            operator = 0;
-            currentNumber = 0;
-            display.value = result.toString();
-        }
+        result = process();
+        resultVal.value = result;
+        input[ 0 ] = result;
+        input[ 1 ] = 0;
+        isValidInput[1] = false;
+        operator = 0;
+        currentNumber = 0;
+        display.value = result.toString();
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "0" || e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4"
+       || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9") {
+        addDigitToCurrentNumber(parseInt(e.key));
+    } else if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+        processOperator(e.key);
+    } else if(e.key === "Enter") {
+        processEqual();
+    }
 });
 
 document.getElementById("btn_C").addEventListener("click", () => {
     reset();
+    document.activeElement.blur();
 });
