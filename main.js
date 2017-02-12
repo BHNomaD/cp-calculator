35b26123ab57971c82d2eef1707ea44e47ced78b
Md Shariful Islam <Md Shariful Islam>
2017-02-12 12:01:16 +0600

    Button CE, Back, +/- Decimal added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const {app, BrowserWindow} = electron
 
 app.on('ready', () => {
-    let win = new BrowserWindow({height:500, width:400, autoHideMenuBar: true})
+    let win = new BrowserWindow({height:580, width:400, autoHideMenuBar: true})
     win.loadURL(`file://${__dirname}/index.html`)
     //win.webContents.openDevTools()
 });
